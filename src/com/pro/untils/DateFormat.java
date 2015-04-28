2543647a984e5a423f694466a0859ddef611faa3
BruceWang <haitao_wang@amway.com>
2015-04-28 14:07:33 +0800

    tt

---
+++ src/com/pro/untils/DateFormat.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.pro.untils;
+
+/**
+ * @author [Bruce AJ83984]
+ * 2015/04/28
+ */
+public class DateFormat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
